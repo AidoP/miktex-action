6376d85fd6a7869baee8620e78e070a127224ef0
AidoP <aidop@me.com>
2021-02-02 22:09:52 +0800

    Don't attempt to use subdirs for output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-find "$1" -name '*.tex' -type f -exec sh -c 'tectonic -X compile {} -o "'"$2/"'$(echo {} | sed -E '\''s/(.*).tex/\1/'\'').pdf"' \;
+find "$1" -name '*.tex' -type f -exec sh -c 'tectonic -X compile {} -o "'"$2"'"' \;
