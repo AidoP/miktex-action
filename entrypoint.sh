7eb5113b9024c795c9eae8a4079ef52596bfb82f
AidoP <aidop@me.com>
2021-03-01 02:48:03 +0800

    no perl regex for grep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -d "$3" ] &&
-    find "$3" -name '*.ml' -type f -exec sh -c 'pygmentize -f tex -o "$(echo "{}" | grep -oP '"'.*(?=\.ml)'"').tex" "{}"' \;
+    find "$3" -name '*.ml' -type f -exec sh -c 'pygmentize -f tex -o "$(echo "{}" | sed s/.ml/.tex/)" "{}"' \;
 find "$1" -name '*.tex' -type f -exec sh -c 'tectonic "{}" -o "'"$2"'"' \;
