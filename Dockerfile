5fa72cd00d2ba79b94378e63b87a63d7f8a03ed0
AidoP <aidop@me.com>
2021-03-01 02:42:17 +0800

    Allow code blocks

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM voidlinux/masterdir-x86_64:20200607RC01
 RUN xbps-install -Syu xbps
-RUN xbps-install -y tectonic
+RUN xbps-install -y tectonic python3-Pygments
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-find "$1" -name '*.tex' -type f -exec sh -c 'tectonic {} -o "'"$2"'"' \;
+[ -d "$3" ] &&
+    find "$3" -name '*.ml' -type f -exec sh -c 'pygmentize -f tex -o "$(echo "{}" | grep -oP '"'.*(?=\.ml)'"').tex" "{}"' \;
+find "$1" -name '*.tex' -type f -exec sh -c 'tectonic "{}" -o "'"$2"'"' \;
